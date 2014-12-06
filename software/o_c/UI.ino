5d8b70fb6b23429b51199ff7c4b4e069d4415d62
mxmxmx <mxxstadler@gmail.com>
2014-12-06 19:26:50 +0100

    xxx

---
+++ software/o_c/UI.ino
@@ -0,0 +1,234 @@
+/*
+
+encoders + buttons + ADC stuff
+
+*/
+
+
+extern uint8_t MENU_CURRENT; 
+extern const int16_t param_limits[][2];
+extern const int8_t MAXSCALES;
+extern int16_t SCALE_SEL;
+extern const int8_t MENU_ITEMS;
+
+uint8_t LAST_SCALE = 0;
+uint8_t SCALE_CHANGE = 0;
+
+
+enum encoders {
+  
+  LEFT, RIGHT
+  
+};
+
+enum DISPLAY_PAGE {
+  
+ SCREENSAVER,
+ MENU,
+ CALIBRATE 
+ 
+};  
+
+/* --------------- encoders ---------------------------------------  */
+
+/* encoders variables  */
+uint8_t ENCODER_SWAP, SLOWDOWN = 1; 
+
+/* encoder data */
+int16_t LR_encoderdata[]  = {-999, -999};
+
+/* isr */
+void left_encoder_ISR() {
+  encoder[LEFT].process();
+}
+
+void right_encoder_ISR() {
+  encoder[RIGHT].process();
+}
+
+/*  read */
+void encoders() {
+  
+   int16_t encoderdata; 
+   /* toggle encoder */
+   int8_t thisenc = ~ENCODER_SWAP & 1u; 
+   if (encoder[thisenc].change()) { 
+        
+       encoderdata = encoder[thisenc].pos();
+       if (!thisenc || UImode) { 
+             LAST_UI = millis();
+             UImode = MENU;
+       }
+       LR_encoderdata[thisenc] = encoderdata;  
+   }  
+   ENCODER_SWAP = thisenc;
+}   
+  
+
+/* ---------------  update params ---------------------------------  */  
+
+void update_ENC()  {
+
+  encoders(); 
+  
+  //if (UImode) {
+    
+      int16_t tmp = LR_encoderdata[RIGHT];
+       
+      if (CLK_STATE1) {  CLK_STATE1 = false; _ASR(); }  
+     
+      /* parameters: */
+      if (encoder[RIGHT].pos() != asr_params[MENU_CURRENT]) {    // params changed?
+          int16_t tmp2, tmp3; 
+          tmp2 = param_limits[MENU_CURRENT][0]; // lower limit
+          tmp3 = param_limits[MENU_CURRENT][1]; // upper limit
+           
+          if (tmp < tmp2)      { asr_display_params[MENU_CURRENT] = asr_params[MENU_CURRENT] = tmp2; encoder[RIGHT].setPos(tmp2);}
+          else if (tmp > tmp3) { asr_display_params[MENU_CURRENT] = asr_params[MENU_CURRENT] = tmp3; encoder[RIGHT].setPos(tmp3);}
+          else                 { asr_display_params[MENU_CURRENT] = asr_params[MENU_CURRENT] = tmp;  }
+          MENU_REDRAW = 1;
+         
+      }
+  
+     if (CLK_STATE1) {  CLK_STATE1 = false; _ASR(); }   
+     
+     /* scale select: */   
+     tmp = LR_encoderdata[LEFT]>>SLOWDOWN; 
+     if (tmp != asr_params[0]) {
+           if (tmp >= MAXSCALES)  { SCALE_SEL = 0; encoder[LEFT].setPos(0);}
+           else if (tmp < 0)      { SCALE_SEL = MAXSCALES-1; encoder[LEFT].setPos((MAXSCALES-1)<<SLOWDOWN);}
+           else SCALE_SEL = tmp;
+           MENU_REDRAW = 1;
+           if (LAST_SCALE != SCALE_SEL) SCALE_CHANGE = 1;
+           LAST_SCALE = SCALE_SEL;
+     }
+     else  SCALE_SEL = tmp; 
+  //} 
+}
+
+
+/* --- read  ADC ------ */
+
+void CV() {
+  
+      cvval[0] = (analogRead(CV1));           // sample in
+      cvval[1] = (analogRead(CV2) >> 5) - 63; // index -64/64 
+      cvval[2] = (analogRead(CV3) >> 8) - 7;  // # notes -8/8
+      cvval[3] = (analogRead(CV4) >> 9) - 3;  // octave offset
+      _ADC = false;        
+}
+
+
+/* --- check buttons --- */
+
+void buttons(uint8_t _button) {
+  
+  switch(_button) {
+    
+    
+    case 0: { 
+      
+         if (!digitalReadFast(but_top) && (millis() - LAST_BUT > DEBOUNCE)) { 
+           topButton();
+           LAST_BUT = millis();   
+         } 
+    }
+    
+    case 1: { 
+      
+         if (!digitalReadFast(but_bot) && (millis() - LAST_BUT > DEBOUNCE)) {
+           lowerButton();
+           LAST_BUT = millis();
+         }
+    }
+    
+    case 2: {
+      
+         if (!digitalReadFast(butL) && (millis() - LAST_BUT > DEBOUNCE)) {
+           leftButton(); //
+           LAST_BUT = millis();   
+         }
+    } 
+   
+    case 3: {
+     
+         if (!digitalReadFast(butR) && (millis() - LAST_BUT > DEBOUNCE)) {
+           rightButton(); //
+           LAST_BUT = millis();
+         }      
+    }  
+  } 
+}
+ 
+ 
+/* --------------- button misc ------------------- */
+
+void rightButton() { 
+  
+  if (UImode) update_menu();
+  LAST_UI = millis();
+  UImode = MENU;
+
+} 
+
+/* -----------------------------------------------  */
+
+void leftButton() {
+  
+  if (UImode) update_scale();
+  LAST_UI = millis();
+  UImode = MENU;
+} 
+
+/* -----------------------------------------------  */
+
+void topButton() {
+ 
+   //if (UImode) { 
+         int8_t tmp = asr_params[1];
+         tmp++;
+         if (tmp > 3) tmp = 3; 
+         asr_params[1] = tmp; 
+   //   }
+   //LAST_UI = millis();
+   //UImode = MENU;
+   
+}
+
+/* -----------------------------------------------  */
+
+void lowerButton() {
+
+  // if (UImode) { 
+         int8_t tmp = asr_params[1];
+         tmp--;
+         if (tmp < -3) tmp = -3; 
+         asr_params[1] = tmp; 
+   //    }
+   //LAST_UI = millis();
+   //UImode = MENU;
+   
+}
+
+/* -----------------------------------------------  */
+
+void update_menu(void) {
+ 
+      MENU_CURRENT++;
+      if ( MENU_CURRENT >= MENU_ITEMS) MENU_CURRENT = 2;
+      encoder[RIGHT].setPos(asr_params[MENU_CURRENT]); 
+      MENU_REDRAW = 1;  
+}
+
+/* -----------------------------------------------  */
+
+void update_scale() {
+  
+     asr_params[0] = SCALE_SEL;
+     encoder[LEFT].setPos(SCALE_SEL<<SLOWDOWN); 
+     SCALE_CHANGE = 0;
+     MENU_REDRAW = 1;
+      
+}  
+
+
---
+++ software/o_c/calibrate.ino
@@ -0,0 +1,319 @@
+/*
+
+calibration menu:
+
+enter by pressing left encoder button during start up; follow the instructions
+
+
+*/
+
+#define CALIB_MENU_ITEMS 13
+
+char *calib_menu_strings[CALIB_MENU_ITEMS] = {
+
+   "--> calibrate:", 
+   "trim center value:", 
+   "trim to 1 volts", 
+   "trim to 2 volts", 
+   "trim to 3 volts", 
+   "trim to 4 volts",
+   "trim to 6 volts",
+   "trim to 7 volts",
+   "trim to 8 volts",
+   "trim to 9 volts",
+   "trim to 10 volts",
+   "trim to 0.16667v",
+   "done?"
+};
+
+enum STEPS {
+  
+  HELLO,
+  CENTRE,
+  VOLT_1,
+  VOLT_2,
+  VOLT_3,
+  VOLT_4,
+  VOLT_6,
+  VOLT_7,
+  VOLT_8,
+  VOLT_9,
+  VOLT_10,
+  VOLT_0,
+  CHECK
+};  
+
+
+uint8_t _steps = 0;
+uint16_t DAC_output;
+int32_t encoder_data;
+
+
+/* make DAC table from calibration values */ 
+
+void init_DACtable() {
+  
+  float diff, prev, out;
+  
+  for (int i = 0; i <= OCTAVES; i++) {
+       
+        diff = (float)(octaves[i+1] - octaves[i])/12.0f;
+        
+        for (int j = 0; j <= 11; j++) {
+          
+            semitones[j+i*12] = out = (uint16_t)(0.5 + diff*(j+1) + prev);  
+            if (j >= 11) prev = out; 
+    }
+  }
+  /* deal w/ offset ? */
+  if (octaves[0] != THEORY[0]) {
+    
+        diff = (float)(octaves[1] - octaves[0])/10.0f;
+        semitones[0] = diff;
+        semitones[1] = diff;
+        semitones[2] = diff;
+        for (int j = 3; j < 11; j++) { 
+         
+           semitones[j] = (uint16_t)(0.5 + diff*(j-1));
+        } 
+  } 
+}
+
+/*        loop calibration menu until we're done       */
+
+void calibrate_main() {
+  
+        UImode = CALIBRATE;
+        MENU_REDRAW = 1;
+        _steps = 0;
+        /* values written? */
+        if (EEPROM.read(0x2) > 0) readDACcode();    
+        u8g.setFont(u8g_font_6x12);
+        u8g.setColorIndex(1);
+        u8g.setFontRefHeightText();
+        u8g.setFontPosTop();
+       
+        while(UImode == CALIBRATE) {
+         
+             UI();
+             delay(20);
+             MENU_REDRAW = 1;
+             
+             /* advance in menu or exit */
+             
+             if (_steps >= CHECK) { 
+                      
+                       if (millis() - LAST_BUT > DEBOUNCE && !digitalRead(butR)) {
+                         
+                            UImode = SCREENSAVER;
+                            LAST_BUT = millis();
+                       }
+             } 
+             else if (millis() - LAST_BUT > DEBOUNCE && !digitalRead(butR)) { 
+                       
+                             _steps++;  
+                             if  (_steps == VOLT_0) encoder[RIGHT].setPos(octaves[0]);
+                             else if (_steps == CENTRE) encoder[RIGHT].setPos(octaves[5]);
+                             else if (_steps > CENTRE && _steps < VOLT_6) encoder[RIGHT].setPos(octaves[_steps-1]);
+                             else if (_steps > VOLT_4) encoder[RIGHT].setPos(octaves[_steps]);
+                             LAST_BUT = millis(); 
+             }
+             
+             encoder_data = encoder[RIGHT].pos();
+             if (encoder_data >= MAX_VALUE) encoder[RIGHT].setPos(MAX_VALUE);
+        } 
+        writeDACcode();
+        encoder[RIGHT].setPos(0);
+        UImode = SCREENSAVER;  
+}
+
+
+void calibrate() {
+  
+    u8g.drawStr(10, 10, calib_menu_strings[_steps]); 
+    
+    switch (_steps) {
+      
+      case HELLO: {  // 0
+        
+          u8g.drawStr(10, 30, "use encoder (R)");
+          u8g.drawStr(10, 50, "switch: left pos.");
+          writeAllDAC(THEORY[0]);
+          break;
+      }
+      
+      case CENTRE: {  // 1
+        
+          u8g.drawStr(10, 30, "- > 5.000V /");  
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use trimpots!");  
+          octaves[5] = encoder_data;
+          writeAllDAC(encoder_data);
+          break;   
+      }  
+      
+      case VOLT_1: { // 2
+          u8g.drawStr(10, 30, "- > 1.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps-1] = encoder_data;
+          writeAllDAC(encoder_data);
+          break;   
+      } 
+      
+      case VOLT_2: { // 3
+          u8g.drawStr(10, 30, "- > 2.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps-1] = encoder_data;
+          writeAllDAC(encoder_data);
+          break;   
+      } 
+      
+      case VOLT_3: { // 4
+          u8g.drawStr(10, 30, "- > 3.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps-1] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_4: { // 5
+          u8g.drawStr(10, 30, "- > 4.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps-1] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      }
+      
+      case VOLT_6: { // 6
+          u8g.drawStr(10, 30, "- > 6.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_7: { // 7
+          u8g.drawStr(10, 30, "- > 7.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");
+          octaves[_steps] = encoder_data;  
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_8: { // 8
+          u8g.drawStr(10, 30, "- > 8.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_9: { // 9
+          u8g.drawStr(10, 30, "- > 9.000V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_10: { // 10
+          u8g.drawStr(10, 30, "- > 10.00V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[_steps] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case VOLT_0: { // 11
+          u8g.drawStr(10, 30, "- > .1667V /");
+          u8g.setPrintPos(87, 30);
+          u8g.print(encoder_data);
+          u8g.drawStr(10, 50, "use encoder!");  
+          octaves[0] = encoder_data;
+          writeAllDAC(encoder_data);
+        break;   
+      } 
+      
+      case CHECK: {
+        
+        u8g.drawStr(10, 30, "push to exit");  
+        break;   
+      } 
+      
+      default: break;
+      
+    } 
+} 
+
+
+void writeDACcode() {
+   
+  uint8_t byte0, byte1, adr;
+  uint16_t _DACcode;
+  
+  for (int i = 0; i <= OCTAVES; i++) {  
+    
+       _DACcode = octaves[i];
+       byte0 = _DACcode >> 8;
+       byte1 = _DACcode;
+       EEPROM.write(adr, byte0);
+       adr++;
+       EEPROM.write(adr, byte1);
+       adr++;
+  }  
+}  
+
+
+void readDACcode() {
+  
+   uint8_t byte0, byte1, adr;
+   
+   for (int i = 0; i <= OCTAVES; i++) {  
+  
+       byte0 = EEPROM.read(adr);
+       adr++;
+       byte1 = EEPROM.read(adr);
+       adr++;
+       octaves[i] = (uint16_t)(byte0 << 8) + byte1;
+   }
+}  
+  
+  
+void in_theory() {
+
+   for (int i = 0; i <= OCTAVES; i++) {  
+     
+     octaves[i] = THEORY[i];
+   }
+
+}
+
+
+void writeAllDAC(uint16_t _data) {
+
+  set8565_CHA(_data); // ch A >> 
+  set8565_CHB(_data); // ch B >>
+  set8565_CHC(_data); // ch C >>
+  set8565_CHD(_data); // ch D >>
+  
+}
+  
