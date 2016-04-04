20bd3fd94296e087fc0157c83b411ded64f58fcb
Patrick Dowling <pld@gurkenkiste.com>
2016-04-04 21:26:21 +0200

    [envgen] license

--- software/o_c_REV/APP_ENVGEN.ino
+++ software/o_c_REV/APP_ENVGEN.ino
@@ -1,3 +1,29 @@
+// Copyright (c) 2016 Patrick Dowling, Tim Churches
+//
+// Initial app implementation: Patrick Dowling (pld@gurkenkiste.com)
+// Modifications by: Tim Churches (tim.churches@gmail.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+// Quad enevelope generator app, based on the multistage envelope implementation
+// from Peaks by Olivier Gillet (see peaks_multistage_envelope.h/cpp)
+
 #include "OC_apps.h"
 #include "OC_bitmaps.h"
 #include "OC_digital_inputs.h"
