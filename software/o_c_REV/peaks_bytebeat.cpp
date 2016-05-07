cdfd2205ba0b752a1bd8075537c369c4f108a955
Tim Churches <tim.churches@gmail.com>
2016-05-07 16:15:32 +1000

    [bytebeats] Added 8 more byte beat equations

--- software/o_c_REV/peaks_bytebeat.cpp
+++ software/o_c_REV/peaks_bytebeat.cpp
@@ -91,42 +91,115 @@ uint16_t ByteBeat::ProcessSingleSample(uint8_t control) {
   uint8_t p1 = p1_ ;
   uint8_t p2 = p2_ ;
     switch (equation_index_) {
-        case 0:
+        case 0: // hope
           // from http://royal-paw.com/2012/01/bytebeats-in-c-and-python-generative-symphonies-from-extremely-small-programs/
           // (atmospheric, hopeful)
           sample = ( ( ((t_*3) & (t_>>10)) | ((t_*p0) & (t_>>10)) | ((t_*10) & ((t_>>8)*p1) & p2) ) & 0xFF) << 8;
           break;
-        case 1:
+        case 1: // love
           // equation by stephth via https://www.youtube.com/watch?v=tCRPUv8V22o at 3:38
           sample = ((((t_*p0) & (t_>>4)) | ((t_*p2) & (t_>>7)) | ((t_*p1) & (t_>>10))) & 0xFF) << 8;
           break;
-        case 2: 
+        case 2: // life
           // This one is the second one listed at from http://xifeng.weebly.com/bytebeats.html
           sample = ((( (((((t_ >> p0) | t_) | (t_ >> p0)) * p2) & ((5 * t_) | (t_ >> p2)) ) | (t_ ^ (t_ % p1)) ) & 0xFF)) << 8 ;
           break;
-       case 3:
+       case 3:// age
           // Arp rotator (equation 9 from Equation Composer Ptah bank)
           sample = ((t_>>(p2>>4))&(t_<<3)/(t_*p1*(t_>>11)%(3+((t_>>(16-(p0>>4)))%22))));
           break ;
-        case 4: 
+        case 4: // clysm
           //  BitWiz Transplant via Equation Composer Ptah bank 
           sample = (t_-((t_&p0)*p1-1668899)*((t_>>15)%15*t_))>>((t_>>12)%16)>>(p2%15);
           break ;
-        case 5:
+        case 5: // monk
           // Vocaliser from Equation Composer Khepri bank         
           sample = ((t_%p0>>2)&p1)*(t_>>(p2>>5));
           break;
-        case 6:
+        case 6: // NERV
           // Chewie from Equation Composer Khepri bank         
           sample = (p0-(((p2+1)/t_)^p0|t_^922+p0))*(p2+1)/p0*((t_+p1)>>p1%19);
           break;
-        default:
+        case 7: // Trurl
           // Tinbot from Equation Composer Sobek bank   
           sample = (t_/(40+p0)*(t_+t_|4-(p1+20)))+(t_*(p2>>5));
-          break;      
+          break;
+        case 8: // Pirx  
+          // my loud friend, Ptah bank   
+          sample = (((t_>>((p0>>12)%12))%(t_>>((p1%12)+1))-(t_>>((t_>>(p2%10))%12)))/((t_>>((p0>>2)%15))%15))<<4;
+          break;
+        case 9: //Snaut
+          // GGT2, Ptah bank
+          sample = ((p0|(t_>>(t_>>13)%14))*((t_>>(p0%12))-p1&249))>>((t_>>13)%6)>>((p2>>4)%12);
+           break;
+        case 10: // Hari
+          // burst thinking, Sobek bank
+          sample = (1099991*t_&t_<<(p1-t_%p0)+t_)>>(p2>>6);;
+          break;
+        case 11: // Kelvin
+         // light reactor, Ptah bank
+          sample = ((t_>>3)*(p0-643|(325%t_|p1)&t_)-((t_>>6)*35/p2%t_))>>6;
+          break;
+        case 12: // Tichy
+          // alpha from Khepri bank
+          sample = (((t_^(p0>>3)-456)*(p1+1))/(((t_>>(p2>>3))%14)+1))+(t_*((182>>(t_>>15)%16))&1) ;
+          break;
+        case 13: // Bregg
+          // Triangle wiggler 2 from Khepri bank
+          sample = ((t_>>(p0>>4))|t_|t_>>6)*p2+4*(t_&(t_>>(p1>>4))|t_>>(p0>>4));
+          break;            
+        case 14: // Avon
+          // Widerange from Khepri bank
+          sample = (((p0^(t_>>(p1>>3)))-(t_>>(p2>>2))-t_%(t_&p1)));
+          break;        
+        case 15: // Orac
+          // Timing master from Ptah bank
+          sample = (((t_>>(t_>>(p1%15))%16)/((t_>>((t_>>(p2%15))%15))%12)+p0)*((t_>>(p0%12))+15))<<4;
+          break;
+        default:
+          sample = 0 ;
+          break;          
   }
 #pragma GCC diagnostic pop
   return sample ;
 }
+
+/*
+(t/12 >> 8) & (t/6 >> 7) * t
+
+((t*9 & t>>4) |
+(t*5 & t>>7) |
+(t*3 & t/0x400)) - 1
+
+((t<<1)^((t<<1)+(t>>7)&t>>12))|t>>(7-(1^7&(t>>19)))|t>>8
+
+((t<<2)^((t<<3)&t>>9))|t>>(7-(1^4&(t>>15)))|t>>12 nope
+
+((((((t*2)/1)&128)>>7)*213)+
+((((t*2)/1)&127)/3))+
+((((((t*10)/4)&128)>>7)*213)+
+((((t*10)/4)&127)/3))+
+((((((t*6)/2)&128)>>7)*213)+
+((((t*6)/2)&127)/3)) nope
+                                       
+((t>>1%128)+20)*3*t>>14*t>>18 nope
+
+(t*((t&8192)>>13))+(2*t*(((t+5461)&8192)>>13)) maybe
+                                     
+                                     
+t*(((t>>9)&10)|((t>>11)&24)^((t>>10)&15&(t>>15)))
+
+t*((t>>15)&1) nope
+
+((t>>1%128)+20)*3*t>>14*t>>18 nope
+
+((i<<1)&(i>>4))?0:0xff nope
+
+((i<<((i>>10)&3))&(i>>11)) << (7&(i>>11) nope
+
+((i>>6)^((i>>5+((7&i>>11) == 0))&3 ? 0xaa : 0x00))>>1
+
+((t*("3634589"[t>>13&7]&15))/12&128) + (((((t>>12)^(t>>12)-2)%11*t)/4|t>>13)&127)
+*/
 
 }  // namespace peaks
