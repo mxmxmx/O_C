ae6949b1613c14ef9afe2de2790c03d336bb8741
Tim Churches <tim.churches@gmail.com>
2016-09-28 07:07:28 +1000

    [qq] Move constant sequences to OC_Strings

--- software/o_c_REV/OC_strings.h
+++ software/o_c_REV/OC_strings.h
@@ -18,7 +18,16 @@ namespace OC {
     extern const char * const irrational_sequence_names[];
     extern const char * const irrational_sequence_dirs[];
     extern const char * const trigger_delay_times[kNumDelayTimes];
-  };
+  // Not strings but are constant integer sequences
+  extern const uint8_t pi_digits[256];
+  extern const uint8_t phi_digits[256];
+  extern const uint8_t tau_digits[256];
+  extern const uint8_t eul_digits[256];
+  extern const uint8_t rt2_digits[256];
+  extern const uint8_t dress_digits[256];
+  extern const uint8_t dress31_digits[256];
+  extern const uint8_t dress63_digits[256];
+ };
 
   // Not a string, but needs to be closer to trigger_delay_times
   extern const uint8_t trigger_delay_ticks[];
