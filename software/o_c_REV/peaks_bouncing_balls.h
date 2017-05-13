4038f9f584c2ae8d45944e304b91c077a398e7a2
Tim Churches <tim.churches@gmail.com>
2017-05-14 09:54:24 +1000

    [BBGEN] Add retrigger with CV control

--- software/o_c_REV/peaks_bouncing_balls.h
+++ software/o_c_REV/peaks_bouncing_balls.h
@@ -53,26 +53,27 @@ class BouncingBall {
     gravity_ = 40;
     bounce_loss_ = 4095;
     initial_velocity_ = 0;
+    retrigger_bounces_ = 0 ;
+    bounces_since_trigger_ = 0 ;
+    retrigger_ = false ;
   }
     
   void Configure(int32_t* parameter) {
-    ControlMode control_mode = CONTROL_MODE_FULL;
-    if (control_mode == CONTROL_MODE_HALF) {
-      set_initial_amplitude(65535);
-      set_initial_velocity(0);
-      set_gravity(parameter[0]);
-      set_bounce_loss(parameter[1]);
-    } else {
-      set_gravity(65535 - parameter[0]);
-      set_bounce_loss(parameter[1]);
-      set_initial_amplitude(parameter[2]);
-      set_initial_velocity(parameter[3] - 32768);
-    }
+    set_gravity(65535 - parameter[0]);
+    set_bounce_loss(parameter[1]);
+    set_initial_amplitude(parameter[2]);
+    set_initial_velocity(parameter[3] - 32768);
+    set_retrigger_bounces(parameter[4]);
   }
   
   inline int16_t ProcessSingleSample(uint8_t control) {
     uint16_t const kMaxPos = 32767L ;
-    if (control & CONTROL_GATE_RISING) {
+    if (retrigger_bounces_ && (bounces_since_trigger_ >= retrigger_bounces_)) {
+      retrigger_ = true;
+      bounces_since_trigger_ = 0 ;
+    }
+    if ((control & CONTROL_GATE_RISING) || retrigger_) {
+      retrigger_ = false ;
       if (hard_reset_) {
         velocity_ = initial_velocity_;
         position_ = initial_amplitude_;
@@ -85,7 +86,8 @@ class BouncingBall {
     }
     velocity_ -= gravity_;
     position_ += velocity_;
-    if (position_ < 0) {
+    if (position_ <= 0) {
+      ++bounces_since_trigger_ ;
       position_ = 0;
       velocity_ = -(velocity_ >> 12) * bounce_loss_;
     }
@@ -114,6 +116,16 @@ class BouncingBall {
     initial_velocity_ = static_cast<int32_t>(initial_velocity) << 4; // was 4
   }
 
+  inline void set_retrigger_bounces(uint16_t retrigger_bounces) {
+    retrigger_bounces_ = static_cast<int32_t>(retrigger_bounces) >> 8  ;
+  }
+
+#ifdef BBGEN_DEBUG
+  inline uint16_t get_retrigger_bounces() {
+    return(static_cast<uint16_t>(retrigger_bounces_));
+  }
+#endif // BBGEN_DEBUG
+
   inline void set_hard_reset(bool hard_reset) {
     hard_reset_ = hard_reset;
   }
@@ -121,7 +133,7 @@ class BouncingBall {
   inline bool FillBuffer() const {
     return true;
   }
-  
+
  private:
   int32_t gravity_;
   int32_t bounce_loss_;
@@ -131,7 +143,11 @@ class BouncingBall {
   int32_t velocity_;
   int64_t position_; 
 
+  int32_t retrigger_bounces_ ;
+  int32_t bounces_since_trigger_ ;
+
   bool hard_reset_;
+  bool retrigger_ ;
 
   DISALLOW_COPY_AND_ASSIGN(BouncingBall);
 };
