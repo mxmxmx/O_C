204c5c50b8f069f52d28972401c57bda5d5cd0f4
mxmxmx <stadlmax@wiss-m08.local>
2017-05-19 13:14:39 +0200

    [OC] pull-ups / delay

--- software/o_c_REV/o_c_REV.ino
+++ software/o_c_REV/o_c_REV.ino
@@ -109,6 +109,7 @@ void setup() {
 
   OC::DEBUG::Init();
   OC::DigitalInputs::Init();
+  delay(100); 
   OC::ADC::Init(&OC::calibration_data.adc); // Yes, it's using the calibration_data before it's loaded...
   OC::DAC::Init(&OC::calibration_data.dac);
 
