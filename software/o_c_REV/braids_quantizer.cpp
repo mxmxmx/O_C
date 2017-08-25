134c892dc6a580b29dab5c2fa782f7c1cf8cfcbc
mxmxmx <stadlmax@gess-rz-dock-1-088.ethz.ch>
2017-08-25 12:18:48 +0200

    [QUANTIZER] inline; version

--- software/o_c_REV/braids_quantizer.cpp
+++ software/o_c_REV/braids_quantizer.cpp
@@ -50,48 +50,6 @@ void Quantizer::Init() {
   }
 }
 
-void Quantizer::Configure(
-    const int16_t* notes,
-    int16_t scale_span,
-    size_t num_notes,
-    uint16_t mask) {
-  enabled_ = notes != NULL && num_notes != 0 && scale_span != 0 && (mask & ~(0xffff<<num_notes));
-  if (enabled_) {
-
-    // Build up array that contains only the enabled notes, and use that to
-    // generate the codebook. This avoids a bunch of issues and checks in the
-    // main generating loop.
-    size_t num_enabled_notes = 0;
-    for (size_t i = 0; i < num_notes; ++i) {
-      if (mask & 1)
-        enabled_notes_[num_enabled_notes++] = notes[i];
-      mask >>= 1;
-    }
-    notes = enabled_notes_;
-   
-    int32_t octave = 0;
-    size_t note = 0;
-    int16_t span = scale_span;
-    int16_t *codebook;
-    
-    codebook = &codebook_[0];
-  
-    for (int32_t i = 0; i < 64; ++i) {
-      int32_t up = notes[note] + span * octave;
-      int32_t down = notes[num_enabled_notes - 1 - note] + (-octave - 1) * span;
-      CLIP(up)
-      CLIP(down)
-      *(codebook + 64 + i) = up;
-      *(codebook + 63 - i) = down;
-      ++note;
-      if (note >= num_enabled_notes) {
-        note = 0;
-        ++octave;
-      }
-    }
-  }
-}
-
 int32_t Quantizer::Process(int32_t pitch, int32_t root, int32_t transpose) {
   if (!enabled_) {
     return pitch;
